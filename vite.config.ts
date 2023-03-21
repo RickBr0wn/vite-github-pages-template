122ded01af2b81b6fe9b3f48671a72e27b8ae7c8
Rick Brown <ricky.brown.00@gmail.com>
2023-03-21 13:48:45 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	// edit `base` with the name of the repo to be hosted on gh-pages
-	base: '/vite-project/',
+	base: '/vite-github-pages-template/',
 	plugins: [react()]
 })
